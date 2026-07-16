b7c221bd092a5d73df61561d0b1adf8d77a0724a
Mandu <mandujoshi5@gmail.com>
2026-07-16 13:35:45 +0530

    loops

---
+++ loops.py
@@ -0,0 +1,11 @@
+numbers=(0,1,2,3,4,5)
+for number in numbers:
+    if number==3:
+        continue
+    if number !=5:
+        print(number)
+    else:
+        break
+print("operation is done")
+    
+    
